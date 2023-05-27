aa21e82c21ce07636422f90376cb6af7ddafd64f
Marcella Sojandrimalala <fenoarisoamarcella@gmail.com>
2023-05-27 21:28:34 +0300

    third commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+let right = document.getElementById('chevron_right');
+let left = document.getElementById('chevron_left');
+let scroll = document.querySelector('.scroll');
+let scrollContainer = scroll.parentElement;
+let scrollWidth = scroll.scrollWidth;
+let containerWidth = scrollContainer.offsetWidth;
+let scrollPosition = 0;
+
+right.addEventListener('click', (e) => {
+  scrollPosition += containerWidth;
+  if (scrollPosition > scrollWidth - containerWidth) {
+    scrollPosition = scrollWidth - containerWidth;
+  }
+  scroll.style.transform = `translateX(-${scrollPosition}px)`;
+});
+
+left.addEventListener('click', (e) => {
+  scrollPosition -= containerWidth;
+  if (scrollPosition < 0) {
+    scrollPosition = 0;
+  }
+  scroll.style.transform = `translateX(-${scrollPosition}px)`;
+});
